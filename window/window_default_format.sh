b4684e41f61435dc9566b8404506a97f3b341524
jo-project <jo.project.0911@gmail.com>
2023-09-27 20:20:01 +1000

    Catppuccino

--- window/window_default_format.sh
+++ window/window_default_format.sh
@@ -1,9 +1,8 @@
 show_window_default_format() {
   local number="#I"
-  local host="#H"
   local color="$thm_blue"
   local background="$thm_bg"
-  local text="$(get_tmux_option "@catppuccin_window_default_text" "host")" # use #W for application instead of directory
+  local text="$(get_tmux_option "@catppuccin_window_default_text" "#H")" # use #W for application instead of directory
   local fill="$(get_tmux_option "@catppuccin_window_default_fill" "number")" # number, all, none
   
   local default_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
