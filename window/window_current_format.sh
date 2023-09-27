cda069f5586e157dcb3ad7236095b255e6af352e
jo-project <jo.project.0911@gmail.com>
2023-09-27 20:01:01 +1000

    Catppuccino

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -1,13 +1,11 @@
 show_window_current_format() {
-    local number="#I"
-    local color="$thm_mauve"
-    local background="$thm_black"
+  local number="#I"
+  local color="$thm_mauve"
+  local background="$thm_bg"
+  local text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  local fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")" # number, all, none
 
-    local text="$(get_tmux_option "@catppuccino_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  local current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
 
-    local fill="$(get_tmux_option "@catppuccino_window_current_fill" "number")" # number, all, none
-
-    local current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
-
-    echo "$current_window_format"
+  echo "$current_window_format"
 }
